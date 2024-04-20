72c3c6c6affe38844a8fcae11ecb6fd5ae857ff6
runo saduwa <orunosaduwa@gmail.com>
2024-04-20 08:51:25 +0100

    updates

--- drills/10-advanced-typing/solution.ts
+++ drills/10-advanced-typing/solution.ts
@@ -1,3 +1,12 @@
 // ⚠️ SOLUTION ⚠️
 //  Exercise 10: Advanced Typing
 // Create a mapped type that takes an interface and produces a new interface with all properties of the original interface set to optional.
+
+interface Person {
+  name: string;
+  email?: string;
+}
+
+type Optional<T> = {
+  [P in keyof T]?: T[P]
+};
